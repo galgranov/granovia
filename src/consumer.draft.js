77573550b2b15eae28f882377faa2bfe239fccef
gal granov <gal.granov@gemalto.com>
2019-06-02 11:06:31 +0300

    feat: ff
    init coding

---
+++ src/consumer.draft.js
@@ -0,0 +1,3 @@
+const m = require('./index.js');
+m.slugify('gal gal testing the test')
+
---
+++ src/index.js
@@ -0,0 +1,23 @@
+
+const marker = require('@ajar/marker'); 
+
+function slugify(slug) {
+    const a = 'àáäâãåăæçèéëêǵḧìíïîḿńǹñòóöôœøṕŕßśșțùúüûǘẃẍÿź·/_,:;'
+    const b = 'aaaaaaaaceeeeghiiiimnnnooooooprssstuuuuuwxyz------'
+    const p = new RegExp(a.split('').join('|'), 'g')
+    let res= slug.toString().toLowerCase()
+      .replace(/\s+/g, '-') // Replace spaces with -
+      .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+      .replace(/&/g, '-and-') // Replace & with ‘and’
+      .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+      .replace(/\-\-+/g, '-') // Replace multiple - with single -
+      .replace(/^-+/, '') // Trim - from start of text
+      .replace(/-+$/, '') // Trim - from end of text
+
+      marker.info(res);
+      marker.info(slug);
+
+
+  }
+
+  module.exports = {slugify}
